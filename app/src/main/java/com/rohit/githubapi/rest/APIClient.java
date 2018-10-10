e64371a82dc3a7d635e7745f60f8aef324d4a482
Rohit Kumar <rohit9075@gmail.com>
2018-10-10 11:54:45 +0530

    API client

--- app/src/main/java/com/rohit/githubapi/rest/APIClient.java
+++ app/src/main/java/com/rohit/githubapi/rest/APIClient.java
@@ -10,12 +10,14 @@ public class APIClient {
 
 
     public static Retrofit getClient() {
-        if (retrofit==null) {
+
+        if (retrofit ==  null){
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
+
         return retrofit;
     }
 }
